4cae2c75e72e0bf1702d7f9f9eab0e57cb77e4d0
barbox11 <123384500+barbox11@users.noreply.github.com>
2024-06-15 08:56:01 -0500

    ejejcicio relizado con dependecia

---
+++ dependencia.js
@@ -0,0 +1,30 @@
+class Carro{
+    constructor(marca, modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.estado = 'funcionando';
+
+    }
+    mostrardetalle(){
+        console.log (`marca ${this.marca}, modelo ${this.modelo}, estado: ${this.estado}`);
+    }
+    cambiarestado(){
+        this.estado = nuevoestado;
+    }
+}
+class serviciomecanico{
+    reparar(carro){
+        console.log(`reparando el ${this.carro.marca} ${this.carro.modelo}`);
+        carro.cambiarestado('reparar');
+        console.log(`reparando el ${carro.marca} ${carro.modelo} ha sido revisado y esta ${carro.estado}`);
+    }
+}
+
+const micarro =new Carro ('toyota', 'hilux');
+
+const taller = new serviciomecanico('');
+
+taller.reparar (micarro);
+
+
+micarro.mostrardetalle(); // muestra marca: toyota, modelo: hilux, estado: reparado
